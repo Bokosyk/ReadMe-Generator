62cddbbc888957615f6c988826a37fffa7ca8df0
Leandro Parrado <lparrado321@gmail.com>
2021-01-17 15:08:02 -0500

    Added packages needed and started array of questions in index.js.

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
+
+const writeFileAsync = util.promisify(fs.writeFile);
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () =>
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?',
+        },
+    ]);
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
