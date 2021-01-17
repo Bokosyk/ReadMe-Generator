d9e5eacfac0c24a6f4ee57fafa2fca74b38e2c6a
Leandro Parrado <lparrado321@gmail.com>
2021-01-17 16:31:52 -0500

    wrote down questions. Created function that writes ReadME file.

--- index.js
+++ index.js
@@ -10,16 +10,102 @@ const questions = () =>
     inquirer.prompt([
         {
             type: 'input',
-            name: 'name',
-            message: 'What is your name?',
+            name: 'title',
+            message: 'What is the title of your project?',
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Write a description of your project.',
+        },
+        {
+            type: 'input',
+            name: 'install',
+            message: 'How do you install your project?',
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'How do you use your project?',
+        },
+        {
+            type: 'input',
+            name: 'credits',
+            message: 'List any other contributors to your project.',
+        },
+        {
+            type: 'input',
+            name: 'license',
+            message: 'Under what license is your project?',
+        },
+        {
+            type: 'input',
+            name: 'badges',
+            message: 'What badges do you have? Type N/A if inapplicable.',
+        },
+        {
+            type: 'input',
+            name: 'features',
+            message: 'What features does your project have?',
+        },
+        {
+            type: 'input',
+            name: 'contribute',
+            message: 'Type N/A if inapplicable.',
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'Write tests and provide examples on how to run them. N/A if inapplicable.',
         },
     ]);
 
+const generateRM = (answers) =>
+`# Your Project Title
+
+## Description 
+
+## Table of Contents (Optional)
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [Credits](#credits)
+* [License](#license)
+
+
+## Installation
+
+## Usage 
+
+## Credits
+
+
+## License
+
+
+---
+
+## Badges
+
+## Features
+
+## Contributing
+
+
+## Tests
+
+
+---
+
+© 2020. All Rights Reserved.`;
+
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+promptUser().then((answers) => writeToFile('README.md', generateRM(answers)))
+.then(() => console.log('Successfully wrote to README.md'))
+.catch((err) => console.error(err));
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
